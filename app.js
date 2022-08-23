51f3e966ef8f141c814efdf05994d658ee5f7740
soukainna <soukaina1lafdili@gmail.com>
2022-08-23 13:55:15 +0200

    add info to my page

--- app.js
+++ app.js
@@ -17,5 +17,9 @@ document.querySelector('#form').addEventListener('submit', function(e){
     .then(res => res.json()
     .then(data => {
         console.log(data)
-    }))
+        document.querySelector('#city').innerHTML = '<i class="fa-solid fa-location-dot"></i>' +'&nbsp;&nbsp;' + data.name
+        document.querySelector('#temp').innerHTML = '<i class="fa-solid fa-temperature-low"></i>' + '&nbsp;&nbsp;' + data.main.temp + ' °'
+        document.querySelector('#humidity').innerHTML = '<i class="fa-solid fa-droplet"></i>' + '&nbsp;&nbsp;' + data.main.humidity + ' %'
+        document.querySelector('#wind').innerHTML = '<i class="fa-solid fa-wind"></i>' + '&nbsp;&nbsp;' + data.wind.speed + ' km/h'
+    })).catch(err => console.log(err))
 })
